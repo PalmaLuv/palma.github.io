28c437ea95ff3200ac204a3b46283fdfa03cbe0e
Palma.luv <84909252+Palma-IT-DUO@users.noreply.github.com>
2023-03-03 17:07:11 +0200

    new site

--- js/script.js
+++ js/script.js
@@ -1,17 +1,48 @@
-window.onload = function () {
-    document.body.classList.add('loaded_hiding');
-    window.setTimeout(function () {
-      document.body.classList.add('loaded');
-      document.body.classList.remove('loaded_hiding');
-    }, 500);
-  }
+const btnDarkMode = document.querySelector(".dark-mode-btn");
 
-  const frames = ['( o.o)', '( -.-)','( o.o)'];
-  setInterval(
-    () => {
-      const frame = frames.shift();
-      document.title = frame;
-      frames.push(frame);
-    },
-    6000,
-  );
+if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches ) {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+}
+
+if (localStorage.getItem('darkMode') == 'dark') {
+    btnDarkMode.classList.add("dark-mode-btn--active");
+    document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") == "light") {
+    btnDarkMode.classList.remove("dark-mode-btn--active");
+    document.body.classList.remove("dark");
+}
+
+window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+        const newColorScheme = event.matches ? "dark" : "light";
+
+        if (newColorScheme == "dark") {
+            btnDarkMode.classList.add("dark-mode-btn--active");
+            document.body.classList.add("dark")
+            localStorage.setItem("darkMode", "dark");
+        } else {
+            btnDarkMode.classList.remove("data-mode-btn--active");
+            document.body.classList.remove("dark");
+            localStorage.setItem("darkMode", "light");
+        }
+    });
+
+btnDarkMode.onclick = function () {
+    btnDarkMode.classList.toggle("dark-mode-btn--active");
+    const isDark = document.body.classList.toggle("dark");
+
+    if (isDark) {
+        localStorage.setItem("darkMode", "dark");
+    } else {
+        localStorage.setItem("darkMode", "light");
+    }
+};
+
+if ( someValue === userValue) {
+    console.log('True')
+} else {
+    console.log('False')
+}
+
