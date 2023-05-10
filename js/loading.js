1b3f1fbf6eca9b95da3c62d07f52dcfe3f232c51
Alexander Kolyasa <mrkyt875@gmail.com>
2023-05-10 18:13:10 +0300

    Update loading.js

--- js/loading.js
+++ js/loading.js
@@ -5,14 +5,16 @@ window.onload = function () {
   }, 500);
 
   var headerWrapper = document.querySelector('.header__wrapper');
-  if (headerWrapper.complete) {
+  headerWrapper.addEventListener('load', function () {
     window.clearTimeout(timeoutId);
+  });
+
+  var isContentLoaded = document.readyState === 'complete';
+  var isHeaderWrapperLoaded = headerWrapper.complete || headerWrapper.readyState === 4;
+  if (isContentLoaded && !isHeaderWrapperLoaded) {
+    clearTimeout(timeoutId);
+    document.body.querySelector('loaded');
     document.body.classList.add('loaded_hiding');
-  } else {
-    headerWrapper.addEventListener('load', function () {
-      window.clearTimeout(timeoutId);
-      document.body.classList.add('loaded_hiding');
-    });
   }
 }
 
