7213b12e165b057a33454525d633643d6993b6e5
Alexander Kolyasa <mrkyt875@gmail.com>
2023-05-10 16:47:09 +0300

    website redesign
    Links, site loading, and the addition of a "post" page have been changed for further purposes.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<h2 align="center">site Palmaluv.live</h2>
+
 Link: [palmaluv.live](https://palmaluv.live/) 
+
+This site was made for his needs and placing it as his personal portfolio.
--- js/loading.js
+++ js/loading.js
@@ -1,10 +1,39 @@
 window.onload = function () {
-    document.body.classList.add('loaded_hiding');
+  var timeoutId = window.setTimeout(function () {
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
+  }, 500);
+  
+  var headerWrapper = document.querySelector('.header__wrapper');
+  headerWrapper.addEventListener('load', function () {
+    window.clearTimeout(timeoutId);
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
+  });
+  
+  if (headerWrapper.complete) {
+    headerWrapper.dispatchEvent(new Event('load'));
+  }
+  
+  var loadedElement = document.querySelector('.loaded');
+  loadedElement.addEventListener('transitionend', function() {
+    loadedElement.classList.remove('loaded');
+  });
+}
+
+var loadedElement = document.querySelector('.loaded');
+loadedElement.addEventListener('transitionend', function() {
+  loadedElement.classList.remove('loaded');
+});
+
+/*window.onload = function () {
+  document.body.classList.add('loaded_hiding');
+    
     window.setTimeout(function () {
       document.body.classList.add('loaded');
       document.body.classList.remove('loaded_hiding');
     }, 500);
-  }
+  }*/
 
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
